035d0c9482adb4c23cd5921f8af78f7b1792880a
BenC <bcuttance1998@gmail.com>
2023-06-16 16:05:12 +0930

    fixed update routes for tags and catergorys

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -48,12 +48,12 @@ router.post('/', async (req, res) => {
 router.put('/:id', async (req, res) => {
   // update a category by its `id` value
   try {
-    const categoryData = await Category.update({
+    const categoryData = await Category.update( req.body, {
       where: {
         id: req.params.id
       }
     })
-    if (!catergoryData){
+    if (!categoryData){
       res.status(404).json({ message: 'No category found with this id!' });
       return;
     }
